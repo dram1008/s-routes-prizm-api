1940ff098148796f8a63ddb5b5969038979a794b
ramha <dram1008@yandex.ru>
2020-09-01 20:48:53 +0300

    2174 Проверить призм ноду

--- config/web.php
+++ config/web.php
@@ -1,7 +1,6 @@
 <?php
 
 $params = require __DIR__ . '/params.php';
-$db = require __DIR__ . '/db.php';
 
 $config = [
     'id'         => 'basic',
@@ -42,7 +41,6 @@ $config = [
                 ],
             ],
         ],
-        'db'           => $db,
         'urlManager'   => [
             'enablePrettyUrl'     => true,
             'showScriptName'      => false,
