d9db58cdad086b4a4654fdcdb6108b4a6ee49424
Neiron <neironcoin@zohomail.eu>
2021-03-24 23:37:08 +0300

    1

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -171,7 +171,7 @@ class SiteController extends Controller
      */
     public function actionIndex2()
     {
-        $client = new Client(['baseUrl' => 'http://localhost:7742']);
+        $client = new Client(['baseUrl' => 'http://localhost:9976']);
         $params = Yii::$app->request->post();
         $response = $client->get('prizm', $params)->send();
 
