4561622d675d45bac85c2576f8d28c2f5389c150
Neiron <neironcoin@zohomail.eu>
2021-03-24 22:13:31 +0300

    1

--- config/params.php
+++ config/params.php
@@ -4,5 +4,5 @@ return [
     'adminEmail'  => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName'  => 'Example.com mailer',
-    'key'         => 'HfighqxCjiHxGIpeXz3wuoMjOIVJSANe7zKAQR03DqSo3J2lwO',
+    'key'         => env('PRIZM_API_KEY'),
 ];
--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,18 @@
 <?php
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../helpers/helpers.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG'));
+defined('YII_ENV') or define('YII_ENV', env('YII_ENV'));
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require __DIR__ . '/../config/web.php';
 
-(new yii\web\Application($config))->run();
+$application = new yii\web\Application($config);
+$application->run();
+
