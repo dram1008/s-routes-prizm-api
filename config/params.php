6142c3f14bc06d86f8ed544edaf6f30d19df9eee
Neiron <neironcoin@zohomail.eu>
2021-06-16 23:18:04 +0300

    https://sroutes.atlassian.net/browse/SR-909

--- config/params.php
+++ config/params.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    'adminEmail'  => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName'  => 'Example.com mailer',
-    'key'         => env('PRIZM_API_KEY'),
+    'adminEmail'          => 'admin@example.com',
+    'senderEmail'         => 'noreply@example.com',
+    'senderName'          => 'Example.com mailer',
+    'key'                 => env('PRIZM_API_KEY'),
+    'prizm_wallet_secret' => env('PRIZM_WALLET_SECRET'),
 ];
--- controllers/RequestController.php
+++ controllers/RequestController.php
@@ -35,6 +35,64 @@ class RequestController extends Controller
      */
     public function actionIndex()
     {
+        $params = Yii::$app->request->post();
+        unset($params['key']);
+
+        return $this->send($params);
+    }
+
+    /**
+     * Выводит монеты на нужный кошелек
+     *
+     * @return string
+     * @throws
+     */
+    public function actionWithdraw()
+    {
+        $params = Yii::$app->request->post();
+        unset($params['key']);
+
+        return $this->payPZM($params['amount'], $params['address'], $params['public_key'], $params['comment']);
+    }
+
+    /**
+     * @param string $summa         сумма для перевода
+     * @param string $pzm           Кошелек для вывода
+     * @param string $public_key    Публичный ключ
+     * @param string $text          Коментарий
+     */
+    private function payPZM($summa, $pzm, $public_key, $text)
+    {
+        $p2 = \Yii::$app->params['prizm_wallet_secret'];   //  это пароль вы который указывали при настройке сервлета
+        $params = [
+            'sendkey'     => $p2,
+            'amount'      => $summa,
+            'comment'     => urlencode($text),
+            'destination' => $pzm,
+            'publickey'   => $public_key,
+            'requestType' => 'sendMoney',
+        ];
+//        $url = 'http://localhost:8888/send?sendkey=' . $p2 . '&amount=' . $summa . '&comment=' . urlencode($text) . '&destination=' . $pzm . '&publickey=' . $public_key;
+//        $page = '';
+//        $result = get_web_page($url);
+//
+//        if (($result['errno'] != 0) || ($result['http_code'] != 200)) {
+//            $error = $result['errmsg'];
+//        } else {
+//            $page = $result['content'];
+//        }
+//
+//        if (preg_match('/^\+?\d+$/', $page)) {
+//            $return = true;
+//        } else {
+//            $return = false;
+//        }
+
+        return $this->send($params);
+    }
+
+    private function send($params)
+    {
         $arrContextOptions = [
             "ssl" => [
                 "verify_peer"      => false,
@@ -42,8 +100,6 @@ class RequestController extends Controller
             ],
         ];
 
-        $params = Yii::$app->request->post();
-        unset($params['key']);
         $rows = [];
         foreach ($params as $k => $v) {
             $rows[] = $k . '=' . urlencode($v);
@@ -57,6 +113,6 @@ class RequestController extends Controller
             throw $e;
         }
 
-        return  $data;
+        return $data;
     }
 }
